1c6b86b7cb6d67b8ed504434e0e2812583314ab5
king-open <kingwang.0624@gmail.com>
2025-03-01 05:58:26 +0800

    init

---
+++ Instant translation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Instant translation
+//
+//  Created by 不二 Jack on 2025/3/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Instant translation/Instant_translationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instant_translationApp.swift
+//  Instant translation
+//
+//  Created by 不二 Jack on 2025/3/1.
+//
+
+import SwiftUI
+
+@main
+struct Instant_translationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
